629d9463bb7a580bd4dfdbc2d6942f18ec42fac5
santocaridi99 <santocaridi99@gmail.com>
2022-02-04 12:28:56 +0100

    cambiato scrittura chiamata Api

--- src/App.vue
+++ src/App.vue
@@ -24,17 +24,27 @@ export default {
     return {
       // creo un array vuoto di film
       movies: [],
+      // api key che ci servirà per le varie chiamate
+      api_key:'4de8e24617b012aa2d57ca4c9f87b4d5'
     };
   },
   methods: {
     // qui effettuerò chiamata axios di theMovieDatabase Movie/search Api
-    // uso il template literal su get e do alla query un valore inputkeyword
-    // che quando chiamerò il metodo con search avrà il valore della keyword presente nella sezione Headerbox/input
-    // array movies corrisponderà al response data dell'api
+    // quando chiamerò il metodo con search, inputKeyword avrà il valore della keyword presente nella sezione Headerbox/input
+    // dichiaro una variabile params "parametri" a cui assegno come query dell'api la keyword
+    // come api_key assegno valore salvato nei data
+    // e facoltativamente  il linguaggio in italiano .in questo caso 'it'
+    // la chiamata axios avrà parte dell'url base + i parametri assegnati
+    // in questo caso l'array in data movie riceverà dati di result dall'Api
     movieSearch(inputKeyword) {
+      const params = {
+        query:inputKeyword,
+        api_key:this.api_key,
+        language:'it'
+      }
       axios
         .get(
-          `https://api.themoviedb.org/3/search/movie?api_key=4de8e24617b012aa2d57ca4c9f87b4d5&language=it&query=${inputKeyword}&page=1&include_adult=false`
+          `https://api.themoviedb.org/3/search/movie`,{params}
         )
         .then((response) => {
           this.movies=response.data.results
